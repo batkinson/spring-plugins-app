df2dbc5cf8f40bf1adb60f86d4df0981af318558
Brent Atkinson <brent.atkinson@gmail.com>
2015-03-08 12:12:18 -0400

    Added rest endpoint from a plugin.

--- plugins/plugin-two/src/main/java/com/github/batkinson/plugin/PluginService.java
+++ plugins/plugin-two/src/main/java/com/github/batkinson/plugin/PluginService.java
@@ -6,13 +6,21 @@ import com.github.batkinson.plugin.crossplugin.NewService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @org.springframework.stereotype.Service
+@RestController
 public class PluginService implements Service, NewService {
 
     private static Logger log = LoggerFactory.getLogger(PluginService.class);
 
     private ServiceDep munger;
+
+    @RequestMapping(name = "/pluginEndpoint", produces = "text/plain")
+    public String extendIt() {
+        return "Hey!";
+    }
 
     @Autowired
     public void setMunger(ServiceDep munger) {
